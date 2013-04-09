8c7cffe58fadf8d439391df921bb8cabfebd2ded
blue hill <zggjtsgzczh@163.com>
2013-04-09 19:21:45 +0800

    RStudio git upload

--- upload_test_2.R
+++ upload_test_2.R
@@ -6,4 +6,6 @@ user_f <- function(data){
   print ('this is example for function code 4')
   print ('this is example for function code 5')
   print ('this is example for function code 6')
+  print ('this is example for RStudio')
+  print ('this is example for RStduio Git upload')
 }
